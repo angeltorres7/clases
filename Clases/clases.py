95276374c51a27d57c71c6282c1c6ad14a0ffc96
SPLIT_TORRES <torresperezrobertoangel@cetis50cdmx.com>
2025-05-12 16:00:36 -0600

    Update clases.py

--- Clases/clases.py
+++ Clases/clases.py
@@ -1,11 +1,17 @@
+#primero creamos un clase
 class armas:
-    def __init__(self, cañon, culata, cargador, mira, empuñadura):
-       self.cañon = cañon
+    def __init__(self, cañon, culata, cargador, mira, empuñadura):#Definimos los atributos
+       #Ponemos los atributos con el self
+        self.cañon = cañon
        self.culata = culata
        self.cargador = cargador
        self.mira = mira
        self.empuñadura = empuñadura
-       
+
+#Completamos el codigo
 ladra = armas ("reforzado", "pesada", "ampliado 1", "mira opv4", "desenfundado rapido")
 print(ladra.cañon)
 print(ladra.empuñadura)
+#En programación, una clase es una plantilla o modelo para crear objetos. Define las características (atributos) y el comportamiento (métodos) que tendrán los objetos creados a partir de esa clase. Es un concepto fundamental en la programación orientada a objetos 
+
+#Torres Perez Roberto Angel
